00d4828afaaabe03ebbe008eddcbdb29c4670173
hafiz1379 <hafizrasa1379@gmail.com>
2023-09-29 04:03:00 +0430

    Fix linter errors

--- test.rb
+++ test.rb
@@ -2,8 +2,8 @@ require_relative 'my_enumerable'
 require_relative 'my_list'
 list = MyList.new(1, 2, 3, 4)
 puts list.inspect
-puts list.all? { |e| e < 5 }
-puts list.all? { |e| e > 5 }
-puts list.any? { |e| e == 2 }
-puts list.any? { |e| e == 5 }
-puts list.filter { |e| e.even? }.inspect
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+puts list.filter(&:even?).inspect
